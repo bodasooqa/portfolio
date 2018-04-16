2c63638a26e71d81803c060e5ac060f7602e6474
bodasooqa <bodasooqa@gmail.com>
2018-04-16 11:21:30 +0300

    Alert message fix

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -21,21 +21,23 @@ export class BlogComponent implements OnInit {
   }
 
   showAlert() {
-    const alert = document.createElement('div');
-    const par = document.createElement('p');
+    if ($('.alert-msg').length === 0) {
+      const alert = document.createElement('div');
+      const par = document.createElement('p');
 
-    par.innerHTML = 'Извините, блог временно не доступен.';
-    $(alert).append(par);
-    $(alert).addClass('alert-msg');
-    $('body').append(alert);
-    setTimeout(() => {
-      $(alert).css('opacity', '1');
-    }, 10);
-    setTimeout(() => {
-      $(alert).css('opacity', '0');
+      par.innerHTML = 'Извините, блог временно не доступен.';
+      $(alert).append(par);
+      $(alert).addClass('alert-msg');
+      $('body').append(alert);
       setTimeout(() => {
-        $(alert).remove();
-      }, 500);
-    }, 5000);
+        $(alert).css('opacity', '1');
+      }, 10);
+      setTimeout(() => {
+        $(alert).css('opacity', '0');
+        setTimeout(() => {
+          $(alert).remove();
+        }, 500);
+      }, 5000);
+    }
   }
 }
