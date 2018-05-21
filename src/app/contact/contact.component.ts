ca7322a6fd50bc09057e714e6924feba8afdd6ad
bodasooqa <bodasooqa@gmail.com>
2018-05-21 14:46:56 +0300

    Messages complete

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../data.service';
 
+declare var $;
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -26,6 +28,32 @@ export class ContactComponent implements OnInit {
       .subscribe(data => {
         return data;
       });
+    this.message.name = '';
+    this.message.email = '';
+    this.message.subject = '';
+    this.message.text = '';
+  }
+
+  showAlert() {
+    if ($('.alert-msg').length === 0) {
+      const alert = document.createElement('div');
+      const par = document.createElement('p');
+
+      par.innerHTML = 'Ваше сообщение успешно отправлено.';
+      $(alert).append(par);
+      $(alert).addClass('alert-msg');
+      $(alert).addClass('access');
+      $('body').append(alert);
+      setTimeout(() => {
+        $(alert).css('opacity', '1');
+      }, 10);
+      setTimeout(() => {
+        $(alert).css('opacity', '0');
+        setTimeout(() => {
+          $(alert).remove();
+        }, 500);
+      }, 5000);
+    }
   }
 
 }
